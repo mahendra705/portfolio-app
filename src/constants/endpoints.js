06328ed12740f94a3edefc75b32903e689837a57
Mahendra <manishagrawal705@gmail.com>
2024-11-18 20:08:10 +0530

    fix

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,13 +1,13 @@
 const endpoints = {
   navbar: 'profile/navbar.json',
   routes: 'profile/routes.json',
-  home: 'profile/home.json',
-  social: 'profile/social.json',
-  about: 'profile/about.json',
-  skills: 'profile/skills.json',
-  education: 'profile/education.json',
-  experiences: 'profile/experiences.json',
-  projects: 'profile/projects.json',
+  home: 'portfolio-app/profile/home.json',
+  social: 'portfolio-app/profile/social.json',
+  about: 'portfolio-app/profile/about.json',
+  skills: 'portfolio-app/profile/skills.json',
+  education: 'portfolio-app/profile/education.json',
+  experiences: 'portfolio-app/profile/experiences.json',
+  projects: 'portfolio-app/profile/projects.json',
 };
 
 export default endpoints;
